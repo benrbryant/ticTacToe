25db2b2a3f7a9a52847fd7cbdac6caada69bed66
Ben Bryant <59986847+Bryantellius@users.noreply.github.com>
2020-01-28 15:13:01 -0600

    finished makePlay function
    The makePlay() function will now check the square to see if a) there is any content b) if the click count is odd or even c) will place an 'x' or 'o' value depending on the click count.

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 // Selects all squares from document
 let cells = document.querySelectorAll('.row>div');
 
-cells.forEach(function(cells) {
-    addEventListener('click', makePlay);
+// Variable for amount of clicks, odd are 'X', even are 'O'
+let count=0;
+
+// Adds event listener to each square
+cells.forEach(function(div) {
+    div.addEventListener('click', makePlay);    
 });
 
+// function for each play
 function makePlay(e) {
-   e.target.textContent = 'X';
+   if(e.target.textContent=='' && !(count % 2)){
+    e.target.textContent = 'X';
+    count++;
+    console.log(count);
+    }
+    else if(e.target.textContent=='' && count % 2) {
+    e.target.textContent = 'O';
+    count++;
+    console.log(count); 
+    } 
 };
-  
-console.log(cells); 
+
+
   
 
  
